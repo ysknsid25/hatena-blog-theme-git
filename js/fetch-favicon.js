f5682fa32e5c7ee1d700cb6d08e93d9578388b42
ysknsid25 <kengo071225@gmail.com>
2026-01-23 14:51:49 +0900

    feat: add link favicon
    Signed-off-by: ysknsid25 <kengo071225@gmail.com>

---
+++ js/fetch-favicon.js
@@ -0,0 +1,22 @@
+<script>
+document.addEventListener('DOMContentLoaded', function() {
+    const links = document.querySelectorAll('.entry-content a');
+
+    links.forEach(function(link) {
+      if(link.href.startsWith('https://d.hatena.ne.jp/keyword')){
+          return
+      }
+      if (link.hostname && link.href.startsWith('http')) {
+        const domain = link.hostname;
+        const faviconUrl = `https://www.google.com/s2/favicons?domain=${domain}`;
+
+        const img = document.createElement('img');
+        img.src = faviconUrl;
+        img.className = 'link-favicon';
+        img.alt = '';
+        
+        link.prepend(img);
+      }
+    });
+});
+</script>
